3c1d0e362f446cafaec95182ed00b86a15c2201a
Daniele Bertillo <dbertill@e3r7p5.42roma.it>
2021-04-17 12:53:43 +0200

    ft_printf_c fixed now wroking

--- ft_printf_c.c
+++ ft_printf_c.c
@@ -5,26 +5,21 @@ int		ft_c_padding_right(char c, t_flags *flags)
 	int		i;
 	char	*s;
 
-	// if (c == 0)
-	// 	return (ft_c_padding_left(c, flags + 1));
 	s = ft_calloc(flags->width + 1);
 	if (!s)
 		return (-1);
-	// if (c == '\x00')
-	// 	write(1, &c, 1);
-	// else
-	// 	s[0] = c;
-	s[0] = c;
-	i = 1;
-	while (i < flags->width)
+	write(1, &c, 1);
+	i = 0;
+	while (i < flags->width - 1)
 		s[i++] = ' ';
-	return(ft_result(s));
+	return(1 + ft_result(s));
 }
 
 int		ft_c_padding_left(char c, t_flags *flags)
 {
 	int		i;
 	char	*s;
+	int		plusOne;
 
 	s = ft_calloc(flags->width + 1);
 	if (!s)
@@ -33,9 +28,16 @@ int		ft_c_padding_left(char c, t_flags *flags)
 	while (i < flags->width - 1)
 		s[i++] = ' ';
 	s[i] = c;
+	plusOne = 0;
 	if (s[i] == '\x00')
+		plusOne = 1;
+	if (plusOne)
+	{
+		plusOne = ft_result(s);
 		write(1, &s[i], 1);
-	return(ft_result(s));
+		return(1 + plusOne);
+	}
+	return (ft_result(s));
 }
 
 int		ft_printf_c(char c, t_flags *flags)
--- main.c
+++ main.c
@@ -195,16 +195,12 @@ int		main(void)
 	// j = ft_printf("#%9.7s#\n", NULL);
 	// printf("%d = %i\n", i, j);
 
-	// i = printf("#%.*s#\n", 8, NULL);
-	// j = ft_printf("#%.*s#\n", 8, NULL);
-	// printf("%d = %i\n", i, j);
+	i = printf("#hello, %s.#\n", NULL);
+	j = ft_printf("#hello, %s.#\n", NULL);
+	printf("%d = %i\n", i, j);
 
 	// i = printf("#%-*c#\n", 10, '1');
 	// j = ft_printf("#%-*c#\n", 10, '1');
 	// printf("%d = %i\n", i, j);
-
-	i = printf("#%05i#\n", 43);
-	j = ft_printf("#%05i#\n", 43);
-	printf("%d = %d\n", i, j);
 
 }
